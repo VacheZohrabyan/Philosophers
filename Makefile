1c389e572ac29dc8a94e50a194b9e8bced9d8920
Vache Zohrabyan <vzohraby@c2r6s4.42yerevan.am>
2025-07-05 11:41:52 +0400

    s

--- Makefile
+++ Makefile
@@ -6,33 +6,12 @@ CFLAGS = -Wall -Wextra -Werror -g3 -lpthread
 
 HIDDER = inc/philo.h
 
-DIR_SOURCE_FREE = src/free/ft_free_double.c \
-				  src/free/ft_free_fork.c \
-				  src/free/ft_free_stract.c \
-				  src/free/ft_free_single.c \
-				  src/free/ft_free.c
-
-DIR_SOURCE_INITILAIZE = src/initialize/initialize_fork.c \
-						src/initialize/initialize_philo.c \
-						src/initialize/initialize_program.c \
-						src/initialize/initialize.c 
-
-DIR_SOURCE_LIBFT = src/libft/ft_atoi.c 
-				
-DIR_SOURCE_OTHER = src/other/dream.c \
-				   src/other/eat.c \
-				   src/other/ft_check_argument.c \
-				   src/other/ft_is_digit.c \
-				   src/other/ft_print_message.c \
-				   src/other/ft_usleep.c \
-				   src/other/get_current_time.c \
-				   src/other/main.c \
-				   src/other/think.c \
-
-SOURCE = $(DIR_SOURCE_FREE) \
-		 $(DIR_SOURCE_INITILAIZE) \
-		 $(DIR_SOURCE_LIBFT) \
-		 $(DIR_SOURCE_OTHER) \
+SOURCE = src/init.c \
+		 src/main.c \
+		 src/monitor.c \
+		 src/route_action.c \
+		 src/threads.c \
+		 src/utils.c 
 
 COBJS = $(SOURCE:.c=.o)
 
--- inc/philo.h
+++ inc/philo.h
@@ -5,74 +5,71 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: vzohraby <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/16 14:21:56 by vzohraby          #+#    #+#             */
-/*   Updated: 2025/06/19 12:50:59 by vzohraby         ###   ########.fr       */
+/*   Created: 2025/07/05 11:35:46 by vzohraby          #+#    #+#             */
+/*   Updated: 2025/07/05 11:36:34 by vzohraby         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
-#define PHILO_H
+# define PHILO_H
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <unistd.h>
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h>
-#include <sys/time.h>
-
-#define MAX_PHILO 300
+# define PHILO_MAX 300
 
 typedef struct s_philo
 {
-    pthread_t thread;
-    int id;
-    int eating;
-    int meals_eaten;
-    size_t last_meal;
-    size_t time_to_eat;
-    size_t time_to_dead;
-    size_t time_to_sleep;
-    size_t start_time; 
-    int 				num_of_philos;
-	int 				num_times_to_eat;
-    int* dead;
-    pthread_mutex_t *l_lock;
-    pthread_mutex_t *r_lock;
-    pthread_mutex_t *write_lock;
-    pthread_mutex_t *dead_lock;
-    pthread_mutex_t *meal_lock;
-}   t_philo;
-
+	pthread_t		thread;
+	int				id;
+	int				eating;
+	int				meals_eaten;
+	size_t			last_meal;
+	size_t			time_to_die;
+	size_t			time_to_eat;
+	size_t			time_to_sleep;
+	size_t			start_time;
+	int				num_of_philos;
+	int				num_times_to_eat;
+	int				*dead;
+	pthread_mutex_t	*r_fork;
+	pthread_mutex_t	*l_fork;
+	pthread_mutex_t	*write_lock;
+	pthread_mutex_t	*dead_lock;
+	pthread_mutex_t	*meal_lock;
+}					t_philo;
 typedef struct s_program
 {
-    int dead_flag;
-    pthread_mutex_t write_lock;
-    pthread_mutex_t dead_lock;
-    pthread_mutex_t meal_lock;
-    t_philo *philo;
-}   t_program;
-
-int ft_check_argument(char** argv);
-
-int initialize(char** argv, t_program** program, pthread_mutex_t** fork);
-int initialize_program (char **argv, t_program** program, pthread_mutex_t** fork);
-void initialize_philo(char** argv, t_program** program, pthread_mutex_t **fork);
-void initialize_fork(pthread_mutex_t* fork, int size_philo);
-
-size_t ft_atoi(char* str);
-
-int ft_is_digit(char* str);
-
-void ft_free_single(char* str);
-void ft_free_double(char** str);
-void ft_free_struct(t_program **program);
-void ft_free_fork(pthread_mutex_t* fork, int size_philo);
-void ft_free(t_program** program, pthread_mutex_t **fork);
-
-int	ft_usleep(size_t milliseconds);
-size_t get_current_time();
-
-void eat(t_philo* philo);
-void dream(t_philo* philo);
-void think(t_philo* philo);
-void ft_print_message(char* str, t_philo* philo, int id);
+	int				dead_flag;
+	pthread_mutex_t	dead_lock;
+	pthread_mutex_t	meal_lock;
+	pthread_mutex_t	write_lock;
+	t_philo			*philos;
+}					t_program;
+int		check_arg_content(char *arg);
+int		check_valid_args(char **argv);
+void	destory_all(char *str, t_program *program, pthread_mutex_t *forks);
+void	init_program(t_program *program, t_philo *philos);
+void	init_forks(pthread_mutex_t *forks, int philo_num);
+void	init_philos(t_philo *philos, t_program *program, pthread_mutex_t *forks,
+			char **argv);
+void	init_input(t_philo *philo, char **argv);
+int		thread_create(t_program *program, pthread_mutex_t *forks);
+void	*monitor(void *pointer);
+void	*philo_routine(void *pointer);
+void	eat(t_philo *philo);
+void	dream(t_philo *philo);
+void	think(t_philo *philo);
+int		dead_loop(t_philo *philo);
+int		check_if_all_ate(t_philo *philos);
+int		check_if_dead(t_philo *philos);
+int		philosopher_dead(t_philo *philo, size_t time_to_die);
+int		ft_atoi(char *str);
+int		ft_usleep(size_t microseconds);
+int		ft_strlen(char *str);
+void	print_message(char *str, t_philo *philo, int id);
+size_t	get_current_time(void);
 
 #endif
